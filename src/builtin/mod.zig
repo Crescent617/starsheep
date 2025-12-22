54414be7020ea938bfb8c9e1da504341e6b429ce
Crescent617 <lihuaru617@outlook.com>
2025-12-22 12:30:10 +0800

    fix: hostname

--- src/builtin/mod.zig
+++ src/builtin/mod.zig
@@ -147,7 +147,7 @@ fn hostname(alloc: std.mem.Allocator) []const u8 {
     if (!std.process.hasEnvVarConstant("SSH_CONNECTION")) {
         return "";
     }
-    var buf: [std.os.linux.HOST_NAME_MAX]u8 = undefined;
+    var buf: [std.c.HOST_NAME_MAX]u8 = undefined;
     // std.posix.gethostname 在各平台通用
     const name = std.posix.gethostname(&buf) catch "";
     return alloc.dupe(u8, name) catch "";
